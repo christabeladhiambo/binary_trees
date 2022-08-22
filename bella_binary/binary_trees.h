3f134f7429829d08d8657e08828236f8c222b631
christabeladhiambo <99637155+christabeladhiambo@users.noreply.github.com>
2022-08-22 04:39:57 -0700

    Update binary_trees.h

--- bella_binary/binary_trees.h
+++ bella_binary/binary_trees.h
@@ -6,13 +6,13 @@
 #include <stdio.h>
 
 /**
-* struct binary_tree_s - Binary tree node
-*
-* @n: Integer stored in the node
-* @parent: Pointer to the parent node
-* @left: Pointer to the left child node
-* @right: Pointer to the right child node
-*/
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
 struct binary_tree_s
 {
 	int n;
