2d0c8287dd0544a3f5183dc17ae9549651593c41
christabeladhiambo <99637155+christabeladhiambo@users.noreply.github.com>
2022-08-22 05:40:45 -0700

    Update 10-binary_tree_depth.c

--- bella_binary/10-binary_tree_depth.c
+++ bella_binary/10-binary_tree_depth.c
@@ -1,39 +1,20 @@
-nclude "binary_trees.h"
-
-
+#include "binary_trees.h"
 
 /**
-
-  * binary_tree_depth - Mesure the depth of a tree
-
-  *
-
-  * @tree: Pointer to the tree
-
-  *
-
-  * Return: The depth of the tree
-
-  */
-
+* binary_tree_depth - Mesure the depth of a tree
+*
+* @tree: Pointer to the tree
+*
+* Return: The depth of the tree
+*/
 size_t binary_tree_depth(const binary_tree_t *tree)
-
 {
+	int count = 0;
 
-		int count = 0;
+	if (tree == NULL)
+		return (0);
+	if (tree->parent)
+		count++;
 
-
-
-			if (tree == NULL)
-
-						return (0);
-
-				if (tree->parent)
-
-							count++;
-
-
-
-					return (binary_tree_depth(tree->parent) + count);
-
+	return (binary_tree_depth(tree->parent) + count);
 }
