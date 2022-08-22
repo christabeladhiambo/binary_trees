c4bfc3edc763249e14eb14fb87ea7223dddfcfc8
christabel <christabeladhiambo312gmail.com>
2022-08-22 04:50:53 -0700

    Tee node

---
+++ bella_binary/0-binary_tree_node.c
@@ -0,0 +1,51 @@
+nclude "binary_trees.h"
+
+
+
+/**
+
+  * binary_tree_node - Create a node of binary tree struct,
+
+  *                      If no parent are given, create the parent
+
+  *
+
+  * @parent: The parent node
+
+  * @value: The value to fill
+
+  *
+
+  * Return: Pointer to the created node.
+
+  */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+
+{
+
+		binary_tree_t *newNode = NULL;
+
+
+
+			newNode = malloc(sizeof(binary_tree_t));
+
+				if (newNode == NULL)
+
+							return (NULL);
+
+
+
+					newNode->n = value;
+
+						newNode->left = NULL;
+
+							newNode->right = NULL;
+
+								newNode->parent = parent;
+
+
+
+									return (newNode);
+
+}
