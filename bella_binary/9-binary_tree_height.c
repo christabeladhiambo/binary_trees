a72dfef76dd72c9edcf495f1321dffc463926400
christabel <christabeladhiambo312gmail.com>
2022-08-22 05:35:00 -0700

    Tree height

---
+++ bella_binary/9-binary_tree_height.c
@@ -0,0 +1,55 @@
+nclude "binary_trees.h"
+
+
+
+/**
+
+  * binary_tree_height - function that measures the height of a binary tree
+
+  *
+
+  * @tree: pointer to the root node of the tree to measure the height
+
+  *
+
+  * Return:
+
+  *	height of the tree
+
+  *   0 if tree == NULL
+
+  */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+
+{
+
+		int heightLeft = 0, heightRight = 0;
+
+
+
+			if (!tree)
+
+						return (0);
+
+
+
+				if (tree->left)
+
+							heightLeft = 1 + binary_tree_height(tree->left);
+
+
+
+					if (tree->right)
+
+								heightRight = 1 + binary_tree_height(tree->right);
+
+
+
+						if (heightLeft > heightRight)
+
+									return (heightLeft);
+
+							return (heightRight);
+
+}
