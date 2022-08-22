fad5956529d8ba9f142ecaf613fadd0ab3de3adc
christabeladhiambo <99637155+christabeladhiambo@users.noreply.github.com>
2022-08-22 06:08:36 -0700

    Update 18-binary_tree_uncle.c

--- bella_binary/18-binary_tree_uncle.c
+++ bella_binary/18-binary_tree_uncle.c
@@ -1,51 +1,26 @@
-nclude "binary_trees.h"
-
-
+#include "binary_trees.h"
 
 /**
-
-  * binary_tree_uncle - function that finds the uncle of a node
-
-  *
-
-  * @node: pointer to the root node of the tree to measure the height
-
-  *
-
-  * Return:
-
-  *	a pointer to the uncle node
-
-  *	NULL if no uncle
-
-  */
-
+* binary_tree_uncle - function that finds the uncle of a node
+*
+* @node: pointer to the root node of the tree to measure the height
+*
+* Return:
+*	a pointer to the uncle node
+*	NULL if no uncle
+*/
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
-
 {
+	binary_tree_t *tmp;
 
-		binary_tree_t *tmp;
+	if (node && node->parent && node->parent->parent)
+	{
+		tmp = node->parent;
+		node = tmp->parent;
+		if (node->right == tmp)
+			return (node->left);
+		return (node->right);
+	}
 
-
-
-			if (node && node->parent && node->parent->parent)
-
-					{
-
-								tmp = node->parent;
-
-										node = tmp->parent;
-
-												if (node->right == tmp)
-
-																return (node->left);
-
-														return (node->right);
-
-															}
-
-
-
-				return (NULL);
-
+	return (NULL);
 }
