96b65fa9b66e8ae5f938e9b5b789c288dedbb9a4
christabeladhiambo <99637155+christabeladhiambo@users.noreply.github.com>
2022-08-22 05:16:57 -0700

    Update 5-binary_tree_is_root.c

--- bella_binary/5-binary_tree_is_root.c
+++ bella_binary/5-binary_tree_is_root.c
@@ -1,31 +1,16 @@
-nclude "binary_trees.h"
-
-
+#include "binary_trees.h"
 
 /**
-
-  * binary_tree_is_root - Check if a node is a root
-
-  *
-
-  * @node: Node to check
-
-  *
-
-  * Return: 1 if a node is a root, 0 else
-
-  */
-
+* binary_tree_is_root - Check if a node is a root
+*
+* @node: Node to check
+*
+* Return: 1 if a node is a root, 0 else
+*/
 int binary_tree_is_root(const binary_tree_t *node)
-
 {
-
-		if (node == NULL || node->parent)
-
-					return (0);
-
-			else
-
-						return (1);
-
+	if (node == NULL || node->parent)
+		return (0);
+	else
+		return (1);
 }
