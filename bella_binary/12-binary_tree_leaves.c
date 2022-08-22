13068005c8cf0f3e5e7af7c120fc7fca9a620cc9
christabeladhiambo <99637155+christabeladhiambo@users.noreply.github.com>
2022-08-22 05:45:41 -0700

    Update 12-binary_tree_leaves.c

--- bella_binary/12-binary_tree_leaves.c
+++ bella_binary/12-binary_tree_leaves.c
@@ -1,35 +1,18 @@
-nclude "binary_trees.h"
-
-
+#include "binary_trees.h"
 
 /**
-
-  * binary_tree_leaves - Count the number of leaves of a tree
-
-  *
-
-  * @tree: Pointer to the tree
-
-  *
-
-  * Return: The number of leaves
-
-  */
-
+* binary_tree_leaves - Count the number of leaves of a tree
+*
+* @tree: Pointer to the tree
+*
+* Return: The number of leaves
+*/
 size_t binary_tree_leaves(const binary_tree_t *tree)
-
 {
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (1);
 
-		if (!tree)
-
-					return (0);
-
-			if (!tree->left && !tree->right)
-
-						return (1);
-
-
-
-				return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
-
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
