ee93f2ecd6972ecde8876d761d20c0f4bd3da450
christabel <christabeladhiambo312gmail.com>
2022-08-22 06:04:48 -0700

    Tree sibling

---
+++ bella_binary/17-binary_tree_sibling.c
@@ -0,0 +1,53 @@
+nclude "binary_trees.h"
+
+
+
+/**
+
+  * binary_tree_sibling - Return the sibling of a node
+
+  *
+
+  * @node: The node to get the sibbling
+
+  *
+
+  * Return: Pointer to the node sibbling, NULL else
+
+  */
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+
+{
+
+		if (!node || !node->parent)
+
+					return (NULL);
+
+
+
+			if (node->parent->left == node)
+
+					{
+
+								if (node->parent->right)
+
+												return (node->parent->right);
+
+									}
+
+				else
+
+						{
+
+									if (node->parent->left)
+
+													return (node->parent->left);
+
+										}
+
+
+
+					return (NULL);
+
+}
