6b9438da10efc1919e2d83f9960c6e10d61210f8
christabeladhiambo <99637155+christabeladhiambo@users.noreply.github.com>
2022-08-22 05:32:53 -0700

    Update 8-binary_tree_postorder.c

--- bella_binary/8-binary_tree_postorder.c
+++ bella_binary/8-binary_tree_postorder.c
@@ -1,43 +1,22 @@
-nclude "binary_trees.h"
-
-
+#include "binary_trees.h"
 
 /**
-
-  * binary_tree_postorder - Print the binary tree in the postorder
-
-  *
-
-  * @tree: Pointer to the tree
-
-  * @func: Func to use
-
-  *
-
-  * Return: Anything, cause void function
-
-  */
-
+* binary_tree_postorder - Print the binary tree in the postorder
+*
+* @tree: Pointer to the tree
+* @func: Func to use
+*
+* Return: Anything, cause void function
+*/
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
-
 {
+	if (func == NULL)
+		return;
+	if (tree == NULL)
+		return;
 
-		if (func == NULL)
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
 
-					return;
-
-			if (tree == NULL)
-
-						return;
-
-
-
-				binary_tree_postorder(tree->left, func);
-
-					binary_tree_postorder(tree->right, func);
-
-
-
-						func(tree->n);
-
+	func(tree->n);
 }
