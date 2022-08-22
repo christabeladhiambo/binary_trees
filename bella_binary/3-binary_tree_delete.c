4659f3e9e00f6bf7ade099c4e8ece01150a3b40e
christabel <christabeladhiambo312gmail.com>
2022-08-22 05:05:14 -0700

    Delete trees

---
+++ bella_binary/3-binary_tree_delete.c
@@ -0,0 +1,47 @@
+nclude "binary_trees.h"
+
+
+
+/**
+
+  * binary_tree_delete - function that deletes an entire binary tree
+
+  *
+
+  * @tree: The binary tree to delete
+
+  *
+
+  * Return: None
+
+  */
+
+void binary_tree_delete(binary_tree_t *tree)
+
+{
+
+		binary_tree_t *nodeLeft, *nodeRight;
+
+
+
+			if (tree == NULL)
+
+						return;
+
+
+
+				nodeLeft = tree->left;
+
+					nodeRight = tree->right;
+
+
+
+						binary_tree_delete(nodeLeft);
+
+							binary_tree_delete(nodeRight);
+
+
+
+								free(tree);
+
+}
