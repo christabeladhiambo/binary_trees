94366567f81d9b11001314491b203b1378f062e8
christabeladhiambo <99637155+christabeladhiambo@users.noreply.github.com>
2022-08-22 09:41:25 -0700

    Update 104-binary_tree_rotate_right.c

--- bella_binary/104-binary_tree_rotate_right.c
+++ bella_binary/104-binary_tree_rotate_right.c
@@ -1,65 +1,32 @@
-nclude "binary_trees.h"
-
-
+#include "binary_trees.h"
 
 /**
-
-  * binary_tree_rotate_right - Rotate the given tree on the left
-
-  *
-
-  * @tree: Pointer to the tree
-
-  *
-
-  * Return: Pointer to the new root of the tree
-
-  */
-
+* binary_tree_rotate_right - Rotate the given tree on the left
+*
+* @tree: Pointer to the tree
+*
+* Return: Pointer to the new root of the tree
+*/
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
-
 {
+	binary_tree_t *leftNode;
 
-		binary_tree_t *leftNode;
+	if (!tree || !tree->left)
+		return (NULL);
 
-
-
-			if (!tree || !tree->left)
-
-						return (NULL);
-
-
-
-				leftNode = tree->left;
-
-					tree->left = leftNode->right;
-
-						if (tree->left)
-
-									tree->left->parent = tree;
-
-							leftNode->right = tree;
-
-								leftNode->parent = tree->parent;
-
-									tree->parent = leftNode;
-
-										if (leftNode->parent)
-
-												{
-
-															if (leftNode->parent->left == tree)
-
-																			leftNode->parent->left = leftNode;
-
-																	else
-
-																					leftNode->parent->right = leftNode;
-
-																		}
-
-											return (leftNode);
-
+	leftNode = tree->left;
+	tree->left = leftNode->right;
+	if (tree->left)
+		tree->left->parent = tree;
+	leftNode->right = tree;
+	leftNode->parent = tree->parent;
+	tree->parent = leftNode;
+	if (leftNode->parent)
+	{
+		if (leftNode->parent->left == tree)
+			leftNode->parent->left = leftNode;
+		else
+			leftNode->parent->right = leftNode;
+	}
+	return (leftNode);
 }
-
-
