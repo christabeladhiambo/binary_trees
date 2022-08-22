ed755f7c6b4424fd7a3301e4a37fb061087a3899
christabel <christabeladhiambo312gmail.com>
2022-08-22 05:39:55 -0700

    Tree depth

---
+++ bella_binary/10-binary_tree_depth.c
@@ -0,0 +1,39 @@
+nclude "binary_trees.h"
+
+
+
+/**
+
+  * binary_tree_depth - Mesure the depth of a tree
+
+  *
+
+  * @tree: Pointer to the tree
+
+  *
+
+  * Return: The depth of the tree
+
+  */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+
+{
+
+		int count = 0;
+
+
+
+			if (tree == NULL)
+
+						return (0);
+
+				if (tree->parent)
+
+							count++;
+
+
+
+					return (binary_tree_depth(tree->parent) + count);
+
+}
