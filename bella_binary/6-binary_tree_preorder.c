c4a5ea681b0e29f9ea86a3866fb36042374cef9d
christabeladhiambo <99637155+christabeladhiambo@users.noreply.github.com>
2022-08-22 05:21:25 -0700

    Update 6-binary_tree_preorder.c

--- bella_binary/6-binary_tree_preorder.c
+++ bella_binary/6-binary_tree_preorder.c
@@ -1,53 +1,27 @@
-nclude "binary_trees.h"
-
-
+#include "binary_trees.h"
 
 /**
-
-  * binary_tree_preorder - function that goes through a binary tree
-
-  *	using pre-order traversal
-
-  *
-
-  * @tree: pointer to the root node of the tree to traverse
-
-  * @func: pointer to a function to call for each node
-
-  *
-
-  * Return:
-
-  *	None
-
-  */
-
+* binary_tree_preorder - function that goes through a binary tree
+*	using pre-order traversal
+*
+* @tree: pointer to the root node of the tree to traverse
+* @func: pointer to a function to call for each node
+*
+* Return:
+*	None
+*/
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
-
 {
+	binary_tree_t *nodeLeft, *nodeRight;
 
-		binary_tree_t *nodeLeft, *nodeRight;
+	if (tree == NULL || !func)
+		return;
 
+	nodeLeft = tree->left;
+	nodeRight = tree->right;
 
+	func(tree->n);
 
-			if (tree == NULL || !func)
-
-						return;
-
-
-
-				nodeLeft = tree->left;
-
-					nodeRight = tree->right;
-
-
-
-						func(tree->n);
-
-
-
-							binary_tree_preorder(nodeLeft, func);
-
-								binary_tree_preorder(nodeRight, func);
-
+	binary_tree_preorder(nodeLeft, func);
+	binary_tree_preorder(nodeRight, func);
 }
