2963f6244d1f182d7a99c8cdb763cbb0855ea41f
christabeladhiambo <99637155+christabeladhiambo@users.noreply.github.com>
2022-08-22 09:02:42 -0700

    Update 100-binary_trees_ancestor.c

--- bella_binary/100-binary_trees_ancestor.c
+++ bella_binary/100-binary_trees_ancestor.c
@@ -1,61 +1,31 @@
-nclude "binary_trees.h"
-
-
+#include "binary_trees.h"
 
 /**
-
-  * binary_trees_ancestor - function that finds the lowest common ancestor
-
-  *	of two nodes
-
-  *
-
-  * @first: pointer to the first node
-
-  * @second: pointer to the second node
-
-  *
-
-  * Return:
-
-  *	pointer to the lowest common ancestor node
-
-  *	NULL if no common ancestor
-
-  */
-
+* binary_trees_ancestor - function that finds the lowest common ancestor
+*	of two nodes
+*
+* @first: pointer to the first node
+* @second: pointer to the second node
+*
+* Return:
+*	pointer to the lowest common ancestor node
+*	NULL if no common ancestor
+*/
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-
-					const binary_tree_t *second)
-
+			const binary_tree_t *second)
 {
+	const binary_tree_t *second_tmp = second;
 
-		const binary_tree_t *second_tmp = second;
+	if (!first)
+		return (NULL);
 
+	while (second_tmp != NULL)
+	{
+		if (first == second_tmp)
+			return ((binary_tree_t *)first);
 
+		second_tmp = second_tmp->parent;
+	}
 
-			if (!first)
-
-						return (NULL);
-
-
-
-				while (second_tmp != NULL)
-
-						{
-
-									if (first == second_tmp)
-
-													return ((binary_tree_t *)first);
-
-
-
-											second_tmp = second_tmp->parent;
-
-												}
-
-
-
-					return (binary_trees_ancestor(first->parent, second));
-
+	return (binary_trees_ancestor(first->parent, second));
 }
