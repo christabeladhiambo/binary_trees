278263b9932981f8d6d8cc1d3f9443ac5f642c09
christabeladhiambo <99637155+christabeladhiambo@users.noreply.github.com>
2022-08-22 23:19:04 -0700

    Update 0-binary_tree_node.c

--- bella_binary/0-binary_tree_node.c
+++ bella_binary/0-binary_tree_node.c
@@ -1,26 +1,22 @@
 #include "binary_trees.h"
 
 /**
-* binary_tree_node - Create a node of binary tree struct,
-*                      If no parent are given, create the parent
-*
-* @parent: The parent node
-* @value: The value to fill
-*
-* Return: Pointer to the created node.
-*/
+ * binary_tree_node - Creates a binary tree node
+ * @parent: parent node.
+ * @value: value of the new node.
+ * Return: return a pointer to the new node or NULL on failure.
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *newNode = NULL;
+	binary_tree_t *new = malloc(sizeof(binary_tree_t));
 
-	newNode = malloc(sizeof(binary_tree_t));
-	if (newNode == NULL)
+	if (new == NULL)
 		return (NULL);
 
-	newNode->n = value;
-	newNode->left = NULL;
-	newNode->right = NULL;
-	newNode->parent = parent;
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
+	new->n = value;
 
-	return (newNode);
+	return (new);
 }
